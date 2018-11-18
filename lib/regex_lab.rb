282be9579669da7b455e95c613a5584f791e7938
DANIELLE PARK <dpark1008@gmail.com>
2018-11-18 06:33:02 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-if word.match(/\A[aeiou]/i) == nil
-  return false
-else
-  return true
-end
+  if word.match(/\A[aeiou]/i) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+if text.scan()
 end
 
 def words_five_letters_long(text)
